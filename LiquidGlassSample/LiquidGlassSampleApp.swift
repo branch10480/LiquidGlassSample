c865cbfc69b0e19465b991a7df6602232e4a79de
branch10480 <toshiharu.imaeda@gmail.com>
2026-01-03 18:09:54 +0900

    Initial Commit

---
+++ LiquidGlassSample/LiquidGlassSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquidGlassSampleApp.swift
+//  LiquidGlassSample
+//
+//  Created by Toshiharu Imaeda on 2026/01/03.
+//
+
+import SwiftUI
+
+@main
+struct LiquidGlassSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
